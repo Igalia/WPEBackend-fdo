823a0cffef457a8e05f0ec26104a0715b01dcc97
Adrian Perez de Castro <aperez@igalia.com>
2021-04-10 18:29:13 +0300

    Revert "view-backend-exportable-private: Move ViewBackend::m_destroyClientListener"
    This reverts commit d7577891d505a3fb037ec14e54568fe59e310941.
    
    (cherry picked from commit d56b46f3497b3deb304e245ac531828dc0091341)

--- src/view-backend-exportable-private.h
+++ src/view-backend-exportable-private.h
@@ -87,11 +87,7 @@ private:
 
     std::unique_ptr<FdoIPC::Connection> m_socket;
     int m_clientFd { -1 };
-};
-
-struct wl_client_destroy_listener {
-    ViewBackend* backend;
-    struct wl_listener destroyClientListener;
+    struct wl_listener m_destroyClientListener;
 };
 
 struct wpe_view_backend_exportable_fdo {
