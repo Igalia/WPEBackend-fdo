4aa398f16f72201145eb798af8a4d473f8436d39
Adrian Perez de Castro <aperez@igalia.com>
2021-04-14 15:55:59 +0300

    ws: Remove wl_client pointer that can end up dangling
    Instead of trying to keep track of the destruction of the wl_client
    references from ViewBackend::m_client, completely remove the member
    variable and instead:
    
    - In ViewBackend::releaseBuffer() use wl_resource_get_client() to obtain
      the wl_client from the buffer resource. Given that the buffer resource
      has just been used, it is guaranteed that the wl_client it refers to
      should be still valid.
    
    - In ViewBackend::dispatchFrameCallbacks() remove the flush call, and
      instead do it in WS::Surface::dispatchFrameCallbacks(), and pick the
      wl_client from the callback resource objects, if any callback was
      dispatched at all. Again, if any callback was dispatched their
      associated wl_client should be still valid. Skipping the flush if
      no callbacks are dispatched is fine because in that case there would
      not be any messages  pending be sent over the wire anyway to trigger
      dispatching the callbacks in the client side.

--- src/ws.cpp
+++ src/ws.cpp
@@ -519,14 +519,13 @@ void Instance::releaseAudioPacketExport(struct wpe_audio_packet_export* packet_e
     wpe_audio_packet_export_send_release(packet_export->exportResource);
 }
 
-struct wl_client* Instance::registerViewBackend(uint32_t bridgeId, APIClient& apiClient)
+void Instance::registerViewBackend(uint32_t bridgeId, APIClient& apiClient)
 {
     auto it = m_viewBackendMap.find(bridgeId);
     if (it == m_viewBackendMap.end())
         g_error("Instance::registerViewBackend(): " "Cannot find surface with bridgeId %" PRIu32 " in view backend map.", bridgeId);
 
     it->second->apiClient = &apiClient;
-    return wl_resource_get_client(it->second->resource);
 }
 
 void Instance::unregisterViewBackend(uint32_t bridgeId)
