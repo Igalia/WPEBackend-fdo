Igalia/WPEBackend-fdo

commit b51f539a23c511545033f644ec10aeea4db73850

Only delete images in releaseImage
Deleting images is already done in releaseImage. Also deleting in bufferDestroyListenerCallback
resulted in a double free.

src/view-backend-exportable-fdo-egl.cpp
@@ -247,8 +247,6 @@ public:
 
     void releaseImage(struct wpe_fdo_egl_exported_image* image)
     {
-        image->exported = false;
-
         if (image->bufferResource)
             viewBackend->releaseBuffer(image->bufferResource);
         else
@@ -297,9 +295,6 @@ private:
         image = wl_container_of(listener, image, bufferDestroyListener);
 
         image->bufferResource = nullptr;
-
-        if (!image->exported)
-            deleteImage(image);
     }
 };
 

commit 1307fbf83a0e3afe95f1be7fb56f5448218b1d20

REGRESSION(b51f539): Fix memory leak when the buffer is destroyed at view-backend-exportable-fdo-egl
Since commit b51f539 there is a memory leak each time the wl_resource is destroyed.
This can be easily reproduced by repeteadly switching full-screen on/off
(pressing F11 key) with Cog on Weston.

The memory leak is caused because since b51f539 the wpe_fdo_egl_exported_image object
is not cleaned anymore on the bufferDestroyListenerCallback callback.

Commit cb6b86a fixed the leak but introduced crashes on some cases, so it was reverted.

This is a new attempt at fixing this leak, this adds safeguards to ensure that the
image object is not cleaned twice or with the wrong exported status.

Related-to: #73 #175 #176 #178
Related-to: https://github.com/Igalia/cog/issues/538

src/view-backend-exportable-fdo-egl.cpp
@@ -247,9 +247,13 @@ public:
 
     void releaseImage(struct wpe_fdo_egl_exported_image* image)
     {
-        if (image->bufferResource)
-            viewBackend->releaseBuffer(image->bufferResource);
-        else
+        if (!image)
+            return;
+        if (image->exported) {
+            image->exported = false;
+            if (image->bufferResource)
+                viewBackend->releaseBuffer(image->bufferResource);
+        } else
             deleteImage(image);
     }
 
@@ -285,7 +289,6 @@ private:
     {
         assert(image->eglImage);
         WS::instanceImpl<WS::ImplEGL>().destroyImage(image->eglImage);
-
         delete image;
     }
 
@@ -293,8 +296,13 @@ private:
     {
         struct wpe_fdo_egl_exported_image* image;
         image = wl_container_of(listener, image, bufferDestroyListener);
-
-        image->bufferResource = nullptr;
+        // The image object is inmediately destroyed here only if it's not currently
+        // exported, otherwise it's destroyed when the client releases/returns it.
+        if (image->exported) {
+            image->exported = false;
+            image->bufferResource = nullptr;
+        } else
+            deleteImage(image);
     }
 };
 